789f28ac5e4db747278ac8009819dc846e2d84f6
roxz1990 <roxzgaming56@outlook.com>
2025-02-11 09:42:45 +0530

    Update roxz.py

--- roxz.py
+++ roxz.py
@@ -172,7 +172,7 @@ async def run_attack(chat_id, ip, port, duration, context):
     attack_in_progress = True
 
     try:
-        command = f"./venompapa {ip} {port} {duration} {13} {800}"
+        command = f"./venompapa {ip} {port} {duration} {9} {900}"
         process = await asyncio.create_subprocess_shell(
             command,
             stdout=asyncio.subprocess.PIPE,
